3906892a377cae40ff7da10fe478cd839436826c
jmueseslab <JMUESES@bancodebogota.com.co>
2023-02-21 08:57:11 -0500

    Variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+console.log('Hola mundo');
+
+// VARIABLES Y CONSTANTES
+const nombre = 'Jhon Jairo';
+const apellido = 'Mueses'; 
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+
+//var no se debe usar ... 
+
+if(true){
+    let valorDado = 6;
+    const nombre = 'Peter';
+    console.log(valorDado, nombre);
+}
+
+console.log(valorDado);
