26cbeffc37e5ba6fd7bc1b4bdeca510c8e013cd4
jmueseslab <JMUESES@bancodebogota.com.co>
2023-02-24 17:35:10 -0500

    Import, export y funciones comunes de arreglos

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo');
+import {heroes} from './data/heroes';
+
+const getHeroeById = (id)=>{
+    return heroes.find((heroe) => heroe.id ===id);
+}
+
+console.log(getHeroeById(2));
+
+//find() solo es para el primer elemento de la propiedad
+//filter() validar
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner ===owner);
+console.log(getHeroesByOwner('DC'));
