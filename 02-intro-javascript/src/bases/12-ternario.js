f29d661836774467bbb547ed76e4e3c99e47ffc7
jmueseslab <JMUESES@bancodebogota.com.co>
2023-03-03 14:47:04 -0500

    Operador condicional ternario

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,13 @@
+const activo = true;
+
+const mensaje = (activo) ? 'Activo' : 'inactivo';
+const mensaje2 = (activo) && 'Activo';
+// let mensaje = '';
+// if(activo){
+//     mensaje = 'Activo';
+// }else{
+//     mensaje = 'Inactivo';
+// }
+
+console.log(mensaje);
+console.log(mensaje2);
