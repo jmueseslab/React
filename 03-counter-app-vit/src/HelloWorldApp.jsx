5ff03d486a5e65991ac2dfc0eea52b36718defb0
jmueseslab <JMUESES@bancodebogota.com.co>
2023-03-09 08:55:24 -0500

    Nuestro primer componente

---
+++ 03-counter-app-vit/src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+// export function App(){
+//     //document.createElement...
+//     return (<h1>Hola mundo</h1>);
+// }
+
+// export const hola = '123';
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  );
+}
--- 03-counter-app-vit/src/main.jsx
+++ 03-counter-app-vit/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 //import { ReactDOM } from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
 
-function App(){
-    //document.createElement...
-    return (<h1>Hola mundo</h1>);
-}
 
 const divRoot = createRoot(document.getElementById('root'));
 
 divRoot.render(
     <React.StrictMode>
-        <App/>
+        <HelloWorldApp />
     </React.StrictMode>,
 );
