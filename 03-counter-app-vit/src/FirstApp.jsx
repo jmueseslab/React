a2febae67de78b46d33fe03f738792fdfb155658
jmueseslab <JMUESES@bancodebogota.com.co>
2023-03-09 22:13:50 -0500

    Tarea - Crear un nuevo componente

---
+++ 03-counter-app-vit/src/FirstApp.jsx
@@ -0,0 +1,6 @@
+export const FirstApp = () => {
+    return (
+      <h1>componente Jhon Jairo</h1>
+    );
+}
+// rafc => atajo crear component fuction
--- 03-counter-app-vit/src/main.jsx
+++ 03-counter-app-vit/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 //import { ReactDOM } from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 
 const divRoot = createRoot(document.getElementById('root'));
 
 divRoot.render(
     <React.StrictMode>
-        <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>,
 );
