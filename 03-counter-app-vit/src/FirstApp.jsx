8c6d5ffb49c5c8b687a5d2f12367019618d0201d
jmueseslab <JMUESES@bancodebogota.com.co>
2023-03-21 23:25:33 -0500

    Impresion de variables en HTML

--- 03-counter-app-vit/src/FirstApp.jsx
+++ 03-counter-app-vit/src/FirstApp.jsx
@@ -1,9 +1,21 @@
+// const newMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const newMessage = {
+    message: 'Hola mundo',
+    title: 'Jhon Jairo'
+};
+
+const newNombre = () => {
+    return 'Jhon Jairo';
+}
+
 export const FirstApp = () => {
+
     return (
 
         //fragmento
         <>
-            <h1>Jhon Jairo</h1>
+            <h1>Hola mundo {newNombre()}</h1>
+            {/* <code> { JSON.stringify(newMessage) }</code> */}
             <p>soy un parrafo</p>
         </>
     );
