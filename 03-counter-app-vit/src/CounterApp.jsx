6a209122cb5fb50d768484e95553f082c3d92280
jmueseslab <JMUESES@bancodebogota.com.co>
2023-03-28 23:54:38 -0500

    Eventos

--- 03-counter-app-vit/src/CounterApp.jsx
+++ 03-counter-app-vit/src/CounterApp.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import PropsTypes from 'prop-types'; 
 
 export const CounterApp = ({value}) => {
+
+  function handleAdd (event) {
+    console.log(event)
+  }
+
   return (
     //fragmento
     <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2> { value } </h2>
+
+        <button onClick={ handleAdd }>
+          +1
+        </button>
     </>
   )
 }
