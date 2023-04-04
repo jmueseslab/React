d6a67e6d10d851f9926756d97acb51b351bbf0ea
jmueseslab <JMUESES@bancodebogota.com.co>
2023-04-03 23:57:20 -0500

    Segundo test

---
+++ 03-counter-app-vit/tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,11 @@
+import { getSaludo } from '../../src/base-pruebas/02-template-string';
+
+describe('pruebas en 02-template-string', () => { 
+    test('getSaludo debe retornar el nombre "Hola Fernando"', () => {
+
+        const name = 'Fernando';
+        const message = getSaludo( name ); 
+
+        expect( message ).toBe( `hola ${ name }` );
+    });
+});
