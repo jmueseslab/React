f5a173436f461a829c60c7d91bbd9cc47c973104
jmueseslab <JMUESES@bancodebogota.com.co>
2023-04-05 23:08:45 -0500

    toEqual

---
+++ 03-counter-app-vit/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,26 @@
+import { getUser, getUsuarioActivo } from '../../src/base-pruebas/05-funciones';
+
+describe('Pruebas en 05 Funciones', () => {
+    
+    test('getUser debe retornar un objeto', () => { 
+
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+        };
+
+        const user = getUser();
+        expect(testUser).toEqual(user);
+    });
+
+    test('getUsuarioActivo debe de retornar un objeto', () => { 
+
+        const name = 'Jhon';
+
+        const user = getUsuarioActivo(name);
+        expect(user).toEqual({
+            uid: 'ABC567',
+            username: name        
+        });
+    });
+});
